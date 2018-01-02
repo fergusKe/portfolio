588d8d4eab791eba166f1284c56677ebac7888c7
Fergus <fergus.jingtai@gmail.com>
2018-01-02 15:05:19 +0800

    Add ress package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import Home from './containers/Home/Home'
+
 import registerServiceWorker from './registerServiceWorker'
+import 'ress/dist/ress.min.css'
 
 ReactDOM.render(<Home />, document.getElementById('root'))
 registerServiceWorker()
