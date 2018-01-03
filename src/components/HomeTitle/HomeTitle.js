20752737a38b08f683cea942238e2fd9ba97ce1a
Fergus <fergus.jingtai@gmail.com>
2018-01-03 12:35:10 +0800

    Add react-particles-js package

--- src/components/HomeTitle/HomeTitle.js
+++ src/components/HomeTitle/HomeTitle.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import Particles from 'react-particles-js'
 
 class HomeTitle extends Component {
   render() {
+    const particlesStyle = {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }
+
     return (
       <div id="homeTitle" className="section">
-        HomeTitle
+        <Particles
+          style={particlesStyle}
+        />
       </div>
     )
   }
